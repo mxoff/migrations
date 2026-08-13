d737452d1e449c6fb59723e91461d2e472926d76
maksim.shishkov <shishkov.maxim@yandex.ru>
2026-08-13 19:42:16 +0300

    init

---
+++ migrate.go
@@ -0,0 +1,55 @@
+package migrations
+
+import (
+	"errors"
+	"fmt"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+
+	"github.com/golang-migrate/migrate/v4"
+)
+
+func New(dsn string, files string) *Migrate {
+	return &Migrate{
+		dsn:   dsn,
+		files: files,
+	}
+}
+
+type Migrate struct {
+	dsn   string
+	files string
+}
+
+func (r *Migrate) Migrate() error {
+	m, err := migrate.New(r.files, r.dsn)
+	if err != nil {
+		return fmt.Errorf("migrate.New: %w", err)
+	}
+	defer func() {
+		_, _ = m.Close()
+	}()
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("migrate.Up: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Migrate) Force(version int) error {
+	m, err := migrate.New(r.files, r.dsn)
+	if err != nil {
+		return fmt.Errorf("migrate.New: %w", err)
+	}
+	defer func() {
+		_, _ = m.Close()
+	}()
+
+	if err := m.Force(version); err != nil {
+		return fmt.Errorf("migrate.Force(%d): %w", version, err)
+	}
+
+	return nil
+}
